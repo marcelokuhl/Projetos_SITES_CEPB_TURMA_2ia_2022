173a722f0b99e53e12fa252cf81c3a6be58db096
MARCELO GUILHERME KÜHL <68967909+marcelokuhl@users.noreply.github.com>
2022-11-16 09:32:28 -0300

    Add files via upload

---
+++ VITO_QUADRA/main.autor.js
@@ -0,0 +1,68 @@
+let bool = true;
+
+
+function white() {
+        document.querySelector('body').setAttribute('style', 'background-color: white; color: black !important;');
+
+        document.getElementById('nameAndTitle').setAttribute('style', 'background-color: white; color: black !important;')
+
+        document.getElementById('header').setAttribute('style', 'background-color: burlywood;');
+
+        document.getElementById('a1').setAttribute('style', 'color: black !important;');
+        document.getElementById('a2').setAttribute('style', 'color: black !important;');
+
+        document.getElementById('i1').setAttribute('style', 'color: burlywood !important;');
+        document.getElementById('i2').setAttribute('style', 'color: burlywood !important;');
+        document.getElementById('i3').setAttribute('style', 'color: burlywood !important;');
+
+    } 
+
+    function dark() {
+        document.querySelector('body').setAttribute('style', 'background-color: #111111; color: white !important;');
+
+        document.getElementById('nameAndTitle').setAttribute('style', 'background-color: #111111; color: white !important;')
+
+        document.getElementById('header').setAttribute('style', 'background-color: white;');
+
+        document.getElementById('a1').setAttribute('style', 'color: white !important;');
+        document.getElementById('a2').setAttribute('style', 'color: white !important;');
+
+        document.getElementById('i1').setAttribute('style', 'color: white !important;');
+        document.getElementById('i2').setAttribute('style', 'color: white !important;');
+        document.getElementById('i3').setAttribute('style', 'color: white !important;');
+
+    }
+
+    if(!localStorage.getItem('bool')){
+
+        localStorage.setItem('bool', JSON.stringify(bool));
+    
+    } else {
+    
+        bool = JSON.parse(localStorage.getItem('bool'));
+    
+        if(bool == false){
+            dark()
+        }
+    
+    }
+
+    function validation() {
+
+        bool = bool==true ? false : true;
+    
+        if(bool == true) {
+    
+            white();
+    
+        } else if(bool==false) {
+    
+            dark();
+            
+        }
+        
+        localStorage.setItem('bool', JSON.stringify(bool));
+    
+    }
+    
+    document.getElementById('darkmode').addEventListener("click", ()=>{validation()});
